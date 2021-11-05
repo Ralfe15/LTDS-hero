4324198769ba1cce998ffbd3555a902e9664c702
Rafael Schmidt <ralfe.schmidt@gmail.com>
2021-11-05 14:39:26 +0000

    :construction: Lab02 step #6
    Create game class and call it on main

---
+++ src/main/java/Game.java
@@ -0,0 +1,39 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Game {
+    Screen screen;
+    Game(){
+        try {
+            TerminalSize terminalSize = new TerminalSize(40, 20);
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            this.screen = screen;
+            screen.setCursorPosition(null);   // we don't need a cursor
+            screen.startScreen();             // screens must be started
+            screen.doResizeIfNecessary();     // resize screen if necessary
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void draw(){
+        try {
+            screen.clear();
+            screen.setCharacter(30, 10, TextCharacter.fromCharacter('Y')[0]);
+            screen.refresh();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void run(){
+        draw();
+    }
+}
